6efd5bfb929c97e470e03f15e86a84c4f74f5219
Humberkaan Medina <humberkaan@192.168.1.4>
2026-06-01 19:46:32 -0600

    Mostrar citas

--- agenda/views.py
+++ agenda/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render, redirect
 from .forms import CitaForm
+from .models import Cita
 
 
 def agendar_cita(request):
 
     if request.method == 'POST':
+
         form = CitaForm(request.POST)
 
         if form.is_valid():
             form.save()
+
             return redirect('agendar')
 
     else:
         form = CitaForm()
 
+    citas = Cita.objects.all().order_by(
+        'fecha',
+        'hora'
+    )
+
+    context = {
+        'form': form,
+        'citas': citas
+    }
+
     return render(
         request,
         'agenda/agendar.html',
-        {'form': form}
+        context
     )
